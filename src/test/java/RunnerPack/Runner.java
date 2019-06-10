fb66720bbbcd1ff55179512b305af7ce966ea7a6
bloomingstars <swathi.dinakar@gmail.com>
2019-06-10 12:22:38 +0530

    Add files via upload

---
+++ src/test/java/RunnerPack/Runner.java
@@ -0,0 +1,17 @@
+package RunnerPack;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features=//{"@rerun/failedscenario.txt"},//
+{"FeatureFile/exercise.feature"},
+				glue="cucumber.First",
+				//tags={"@search"})
+				plugin={"html:C:\\Users\\swathi.dinakaran\\workspace\\CuCumberFirst\\report.html",
+					"rerun:rerun/failedscenario.txt"})
+public class Runner {
+
+}
